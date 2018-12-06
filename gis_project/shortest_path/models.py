b7f08458ed9750096b4449e48dd210059e0183ff
mateoKutnjak <mateo.kutnjak7@gmail.com>
2018-12-06 11:47:34 +0100

    Point model created, DB table for points created

--- gis_project/shortest_path/models.py
+++ gis_project/shortest_path/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Point(models.Model):
+    name = models.CharField(max_length=128, blank=True, default="Untitled")
+    coordinate_x = models.FloatField(blank=True)
+    coordinate_y = models.FloatField(blank=True)
