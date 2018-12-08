c38b3f71439a2f7e6bf101a1579bc4697a2c06e7
Lovro Bosnar <lovro.bosnar1@gmail.com>
2018-12-08 21:59:53 +0100

    marker text area, marker buttons

--- gis_project/frontend/code.js
+++ gis_project/frontend/code.js
@@ -21,12 +21,17 @@ function initmap(){
 
 }
 
+// right click
 
-// mouse click listener
+// left mouse click listener
 function onMapClick(event){
 
 	// create marker
 	var marker = new L.marker(event.latlng);
+	inserted_points.push(event.latlng);
+	inserted_markers.push(marker)
+
+	printInsertedPoints();
 	
 	// set content
 	marker.bindPopup("Position " + event.latlng.toString());
@@ -34,5 +39,24 @@ function onMapClick(event){
 	// diplay on map
 	map.addLayer(marker);
 
+}
+
+function show_markers(){
+	n_markers = inserted_markers.length
+	for(i = 0; i < n_markers; ++i){
+		map.addLayer(inserted_markers[i])
+	}
+}
+
+function hide_markers(){
+	n_markers = inserted_markers.length
+	for(i = 0; i < n_markers; ++i){
+		map.removeLayer(inserted_markers[i])
+	}
+}
+
+function printInsertedPoints(){
+	
+	var text = document.getElementById("inserted_points_box").value = inserted_points;
 }
 
