66db952e5d99f134f3bcefeb9d063b6df10a0ddd
Sebastian Janko <janko.sebastian@gmail.com>
2019-01-03 12:36:28 +0100

    fixed urls

--- gis_project/shortest_path/static/code.js
+++ gis_project/shortest_path/static/code.js
@@ -125,7 +125,7 @@ function onMapClick(event){
        type: 'POST',
        dataType : "json",
        data: point_data,
-       url: 'http://localhost:8000/points/create',
+       url: 'http://167.99.129.57:8080/points/create',
        success: function(res){
                console.log(res);
                },
@@ -149,7 +149,7 @@ function onMapClick(event){
 function populateMap() {
     $.ajax({
        type: 'GET',
-       url: 'http://localhost:8000/points/list',
+       url: 'http://167.99.129.57:8080/points/list',
        success: function(data){
             $.each(data, function(index, value) {
                 if(value['fields']) {
@@ -248,7 +248,7 @@ function calculate_distance() {
 
     $.ajax({
        type: 'GET',
-       url: 'http://localhost:8000/paths/shortest_for_points',
+       url: 'http://167.99.129.57:8080/paths/shortest_for_points',
        data: {'point_list': JSON.stringify(selected_points) },
        dataType: 'json',
        success: function(data){
