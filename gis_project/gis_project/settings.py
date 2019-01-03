8b8b715729c9c6d3dfe022c074babb1c0c4f4b32
Sebastian Janko <janko.sebastian@gmail.com>
2019-01-03 12:29:47 +0100

    fixed settings

--- gis_project/gis_project/settings.py
+++ gis_project/gis_project/settings.py
@@ -36,13 +36,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shortest_path.apps.ShortestPathConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shortest_path',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'gis_project.urls'
@@ -113,7 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "shortest_path/static/"),
 )
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/sebo/static/'
+CORS_ORIGIN_ALLOW_ALL = True
